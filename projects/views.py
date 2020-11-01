35132b9f97a1f1017f0604266dc2bb0cec13c826
kdebry <debry.kevin@gmail.com>
2020-11-01 02:09:42 +0100

    contact

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from projects.forms import ContactForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        print(form)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.') 
+    return render(request, "contact.html", {'form': form})
+
 
 def project_index(request):
     projects = Project.objects.all()
